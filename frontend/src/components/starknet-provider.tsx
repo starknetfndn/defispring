1fadf23a669a7cc09e61fdc3e0eb7e02aba8046e
Ondřej Sojka <ondrej.sojka@gmail.com>
2024-02-20 05:41:45 +0000

    Update Starknet provider to show Braavos every tiem

--- frontend/src/components/starknet-provider.tsx
+++ frontend/src/components/starknet-provider.tsx
@@ -16,7 +16,7 @@ export function StarknetProvider({ children }: { children: ReactNode }) {
     // Show these connectors if the user has no connector installed.
     recommended: [argent(), braavos()],
     // Hide recommended connectors if the user has any connector installed.
-    includeRecommended: "onlyIfNoConnectors",
+    includeRecommended: "always",
     // Randomize the order of the connectors.
     order: "alphabetical",
   });
